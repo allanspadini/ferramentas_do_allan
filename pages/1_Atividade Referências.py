d40b37d8ddd7a68aec458bbbf8cbfeea44b3a8a0
Allan Segovia Spadini <allan.spadini@gmail.com>
2024-08-14 11:44:25 -0300

    atividade_referencias

---
+++ pages/1_Atividade Referências.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from groq import Groq
+import requests
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+# Entrada do usuário
+endereco = st.text_input("Link:", "https://pt.wikipedia.org/wiki/Ci%C3%AAncia_de_dados")
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+# Adiciona um botão para executar a inferência
+if st.button("Rodar Inferência"):
+
+    # Fazendo a requisição HTTP para obter o conteúdo da página
+    response = requests.get(endereco)
+    # Parsing do conteúdo HTML com BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Extraindo todo o texto da página
+    text = soup.get_text()
+    # Removendo espaços em branco excessivos
+    text = ' '.join(text.split())
+    # Criando o índice e executando a query
+    prompt = f"""
+            Escreva o enderecão da página no formato [Título da página](endereço da página). 
+            Aqui o endereço:{endereco}. 
+            Na linha de baixo escreva um texto de um parágrado em português brasileiro explicando porque o
+            conteúdo a seguir é bom para quem está estudando o tema. Inicie
+            o texto com o símbolo > seguido de espaço e não utilize 
+            palavras como o texto é bom porque, apenas explique porque ele é útil. Conteúdo:{text}
+    """
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-70b-8192",
+    )
+    resposta = chat_completion.choices[0].message.content
+    st.text_area(label="Output Data:", value=resposta, height=350)
