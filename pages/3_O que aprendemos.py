070addb48312a369a2c74b8f7448a83f4c3d36fb
Allan Segovia Spadini <allan.spadini@gmail.com>
2024-08-29 16:33:23 -0300

    atu

--- pages/3_O que aprendemos.py
+++ pages/3_O que aprendemos.py
@@ -10,12 +10,13 @@ genai.configure(
 )
 
 exemplo = '''
-Nessa aula, você aprendeu como:
-
-Instalar a biblioteca de escaneamento de códigos de barras do ML Kit no Android Studio, utilizando uma abordagem desacoplada;
-Configurar o BarcodeScannerOptions para habilitar a detecção de todos os tipos de códigos de barras;
-Preparar a imagem de entrada para o scanner utilizando através do InputImage.fromMediaImage;
-Implementar o processamento da imagem e a detecção de códigos, incluindo como fechar o proxy e adicionar logs para verificar os resultados.
+Nessa aula, você aprendeu a:
+Utilizar alguns hiperparâmetros do XGBoost;
+Experimentar a técnica de busca aleatória pelos melhores hiperparâmetros;
+Experimentar a técnica de busca em grade pelos melhores hiperparâmetros;
+Selecionar os melhores hiperparâmetros;
+Analisar as métricas do modelo ajustado;
+Comparar os resultados dos modelos antes e após o ajuste.
 
 '''
 
@@ -24,7 +25,7 @@ conteudo = st.text_area("Conteúdo:", "Roteiro da aula")
 
 prompt = f"""
 Você é um criador de atividade O que aprendemos da Alura. Eu irei te passar o conteúdo da aula no qual você deve se basear para criar uma atividade o que aprendemos. 
-Nessa atividade devemos ter de 3 a 5 bullets falando um tópico que aprendemos na aula. Cada bullet deve sempre iniciar com um verbo.
+Nessa atividade devemos ter de 3 a 5 bullets falando um tópico que aprendemos na aula. Cada bullet deve sempre iniciar com um verbo e ser finalizado com ponto e vírgula. Além disso, cada bullet deve ser apenas uma frase simples e curta.
 
 Conteúdo da aula:
 
